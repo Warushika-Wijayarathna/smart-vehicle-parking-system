6a0b406f08a6ed47397b33897232004f6ff6a859
Warushika-Wijayarathna <warushikawijayarathna@gmail.com>
2025-06-25 20:07:32 +0530

    changes are made due to inconsistencies

--- vehicle-service/config/config.py
+++ vehicle-service/config/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/spms_vehicle_db")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ashani:FbmdQQcRxAUbTbNq@mini-pos.ubypzkx.mongodb.net/mini-pos-db")
     EUREKA_SERVER = os.getenv("EUREKA_SERVER", "http://localhost:8761/eureka")
     SERVICE_NAME = os.getenv("SERVICE_NAME", "vehicle-service")
     SERVICE_PORT = int(os.getenv("SERVICE_PORT", "5001"))
